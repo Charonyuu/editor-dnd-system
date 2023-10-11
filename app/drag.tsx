64a7e072e89417b111b058bdfd400c84a229f5a6
Charonyuu <bensona1219@gmail.com>
2023-10-11 10:42:28 +0800

    fix error

--- app/drag.tsx
+++ app/drag.tsx
@@ -3,8 +3,7 @@
 import React, { FC } from 'react';
 import DroppableArea from "../components/DroppableArea";
 import DraggableList from '@/components/DraggableList';
-import { tempBoxs } from '@/components/templates';
-import DragComponentProvider, { useDragContext } from '@/components/Provider/DragProvider';
+import DragComponentProvider from '@/components/Provider/DragProvider';
 
 
 
@@ -18,23 +17,12 @@ const Main: FC = () => {
 }
 
 const App: FC = () => {
-  const { components, setComponents } = useDragContext()
 
-  const handleDragStart = (e: React.DragEvent, CompType: string) => {
-    e.dataTransfer.setData("component_type", CompType);
-  };
-
-  const handleDrop = (e: React.DragEvent) => {
-    const CompType = e.dataTransfer.getData("component_type");
-    const Component = { component: tempBoxs[CompType as keyof typeof tempBoxs], id: Date.now(), type: CompType };
-
-    setComponents([...components, Component]);
-  };
 
   return (
     <div className="flex">
-      <DroppableArea clear={() => setComponents([])} onDrop={handleDrop} components={components} />
-      <DraggableList handleDragStart={handleDragStart} />
+      <DroppableArea />
+      <DraggableList />
     </div>
   );
 }
